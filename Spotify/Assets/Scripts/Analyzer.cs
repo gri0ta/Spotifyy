b9cf6c07fd3256e7c7d1c3c76f889ac5600f2a2e
gri0ta <146214932+gri0ta@users.noreply.github.com>
2024-03-20 19:05:28 +0200

    Music analyzer

---
+++ Spotify/Assets/Scripts/Analyzer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Analyzer : MonoBehaviour
+{
+    AudioSource source;
+    public UnityEvent<float>onVolumeChanged;
+    // Start is called before the first frame update
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var samples = new float[735]; //apskaiciuoti muzikos kadrai video kadruose
+        source.clip.GetData(samples,source.timeSamples); //timesamples duoda kurioj vietoj dabar groja muzika
+
+        var sum = 0f;
+        for(int i = 0; i < samples.Length; i++)
+        {
+            sum += Mathf.Abs(samples[i]);
+        }
+
+        var average = sum / samples.Length;
+
+        onVolumeChanged.Invoke(average);
+    }
+}
